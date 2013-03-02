458de9102894f44a51604f9b7877341bebc30020
Vasudev Kamath <kamathvasudev@gmail.com>
2013-03-02 22:27:45 +0530

    Removed the old scripts

--- start-eng-kan-bot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#set -x
-python /home/vasudev/Documents/sanchaya/kannada-dictionary-bot/eng-kan-bot.py
--- stop-en-kan-bot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -x
-PID_FILE=/var/run/eng-kn-bot.pid
-kill -9 `cat $PID_FILE`
