9bfb188c9da227f354b94877c1ee5e2d9286cbb8
Vasudev Kamath <kamathvasudev@gmail.com>
2014-03-23 23:08:05 +0530

    Refactoring of parsers
    We will have single parser for each dictionary sources like wiktionary,
    dictd etc. All will be inside the parsers folder and all implement
    similar interface which can be accessed by bridge irrespective of kind
    of parser.
    
    Signed-off-by: Vasudev Kamath <kamathvasudev@gmail.com>

--- parsers/wiktionary.py
+++ parsers/wiktionary.py
@@ -1,4 +1,10 @@
 from xml.etree import cElementTree as ET
+from BeautifulSoup import BeautifulSoup
+
+try:
+    import simplejson as json
+except:
+    import json
 
 
 class WiktionaryParser:
@@ -17,7 +23,38 @@ class WiktionaryParser:
         self.logger = logger
 
     def get_meaning(self, data):
-        pass
+        self.meanings = {}
+        wtypes = []
+        meanings_list = []
+
+        try:
+            # Remove () from the output
+            tmp = data.lstrip('(').rstrip(')')
+            content = json.loads(tmp)
+
+            html_content = content.get('parse').get('text').get('*')
+            soup = BeautifulSoup(html_content)
+
+            # H3 has word type
+            h3s = soup.findAll("h3")
+            for h3 in h3s:
+                wtypes.append(h3.find("span", {
+                    "class": "mw-headline"
+                    }).string)
+
+            ols = soup.findAll("ol")
+            for ol in ols:
+                def_list = []
+                for li in ol.findAll("li"):
+                    for a in li.findAll("a"):
+                        def_list.append(a.string)
+                meanings_list.append(def_list)
+
+            self.meanings['wtypes'] = wtypes
+            self.meanings['definitions'] = meanings_list
+        except Exception as e:
+            self.logger.errorlogger.exception
+            ('Something went wrong: {0}'.format(e.message))
 
     def prepare_message(self):
         if len(self.meanings) > 0:
