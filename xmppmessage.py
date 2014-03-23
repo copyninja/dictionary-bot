9bd8e23d7852ac346be87e599751689d29602093
Vasudev Kamath <kamathvasudev@gmail.com>
2014-03-23 23:11:41 +0530

    Move XMPP reply message rendering to separate file.
    Signed-off-by: Vasudev Kamath <kamathvasudev@gmail.com>

---
+++ xmppmessage.py
@@ -0,0 +1,44 @@
+from xml.etree import cElementTree as ET
+
+
+class XMPPReplyMessage:
+    __slots__ = ["im_header", "im_footer", "reply"]
+
+    def __init__(self, meanings):
+        self.reply = self._prepare_message(meanings)
+        self.im_header = """
+<html xmlns='http://jabber.org/protocol/xhtml-im'>
+  <body xmlns='http://www.w3.org/1999/xhtml'>
+"""
+        self.im_footer = """
+  </body>
+</html>
+"""
+
+    def _prepare_message(self, meanings):
+        if len(meanings) > 0:
+            reply_body = ''
+            reply_xml = self.xhtml_im_header
+
+            # some time wiktionary does not contain word type
+            # i.e. verb noun etc. Process the below only if its present
+            if len(meanings.get('wtypes')) > 0:
+                i = 0
+                for wtype in self.meanings.get('wtypes'):
+                    reply_body += '\n' + wtype + ': \n'
+                    reply_xml += '<br/><strong>' + wtype + ': </strong><br/>'
+
+                    defs = ','.join(self.meanings.get('definitions')[i])
+                    reply_body += defs
+                    reply_xml += '<p>' + defs + '</p>'
+
+                    i += 1
+            else:
+                defs = ','.join(self.meanings.get('definitions')[0])
+                reply_body += defs
+                reply_xml = '<p>' + defs + '</p>'
+
+            reply_xml += self.xhtml_im_footer
+            ctree = ET.fromstring(reply_xml)
+            if len(reply_body) > 0:
+                return (reply_body, ctree)
